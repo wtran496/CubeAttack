6823fd3c63731f98b6da8ed05107f300ae0fe907
wtran496 <wtran0496@csu.fullerton.edu>
2020-08-02 18:32:49 -0700

    initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health;
+    public float pointsPlayer = 50;
+    public GameObject player;
+
+    public float waitTime;
+    private float currentTime;
+    private bool shot;
+
+    public GameObject bullet;
+    public Transform bulletSpawnPoint;
+    private Transform bulletSpawned;
+    private Transform pistolHolder;
+    
+
+    public void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+        pistolHolder = this.transform.GetChild(0);
+        bulletSpawnPoint = pistolHolder.GetChild(2);
+
+    }
+    public void Update()
+    {
+        if (health <= 0) {
+            Die();
+        }
+
+        this.transform.LookAt(player.transform);
+
+        if (currentTime == 0) {
+            Shoot();
+        }
+        if (shot && currentTime < waitTime) {
+            currentTime += 1 * Time.deltaTime;
+        }
+        if (currentTime >= waitTime) {
+            currentTime = 0;
+        }
+    }
+
+    public void Die() {
+        print("Enemy " + this.gameObject.name + " has died!");
+        player.GetComponent<Player>().points += pointsPlayer;
+        Destroy(this.gameObject);
+    }
+    public void Shoot() {
+        shot = true;
+        bulletSpawned = Instantiate(bullet.transform, bulletSpawnPoint.transform.position, Quaternion.identity);
+        bulletSpawned.rotation = this.transform.rotation;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 7;
+    public GameObject camera;
+    public float points;
+    public GameObject playerObj;
+
+    public GameObject bulletSpawnPoint;
+    public float waitTime;
+    public GameObject bullet;
+
+    public float maxHealth = 50;
+    public float health;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitDist = 0.0f;
+
+        if (playerPlane.Raycast(ray, out hitDist)) {
+            Vector3 targetPoint = ray.GetPoint(hitDist);
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            targetRotation.x = 0;
+            targetRotation.z = 0;
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 7f * Time.deltaTime);
+        }
+
+        //Player Movement
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           // Vector3 jump = Vector3.up * movementSpeed * Time.deltaTime;
+            transform.Translate(Vector3.up * 100 * Time.deltaTime);
+        }
+        //Shoot
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
+        if (health <= 0){
+            Die();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet.transform, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+    }
+
+    public void Die() {
+        print("died");
+    }
+
+}
